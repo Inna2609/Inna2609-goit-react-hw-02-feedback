db26c2727288df57c6136a33ec564d83f438f010
in.niko <inniko9111@gmail.com>
2022-05-11 15:27:26 +0300

    Create Notification

---
+++ src/components/Notification/Notfication.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const NotificationMessage = styled.p`
+  text-align: center;
+  margin-top: 30px;
+  font-size: 24px;
+  font-weight: bold;
+  color: #212121;
+`;
