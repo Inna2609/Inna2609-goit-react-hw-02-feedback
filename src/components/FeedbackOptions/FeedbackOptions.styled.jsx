af97e97f7a744df5b28bf3c44aaa6f63e23feb11
in.niko <inniko9111@gmail.com>
2022-05-11 15:26:32 +0300

    Create FeedbackOptions

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.button`
+  padding: 8px 32px;
+  font-size: 15px;
+  text-transform: uppercase;
+  cursor: pointer;
+  color: white;
+  background-color: blue;
+  border-radius: 3px;
+  border: 1px solid transparent;
+  box-shadow: 0px 0px 4px 4px rgba(183, 189, 70, 1);
+  :not(:last-child) {
+  margin-right: 8px;
+  }
+  }
+`;
